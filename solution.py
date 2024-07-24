9d233bc82bfe08a4d95bd94787e6cc0e527d332e
mashkoilya <mashkoilya@ecs.vuw.ac.nz>
2024-07-24 13:11:14 +1200

    data filtering finished. rearranged the dates to conform to the format of all the others, but only where day and month were differentiable. removed all others from the data

--- solution.py
+++ solution.py
@@ -17,8 +17,8 @@ def loadData():
     print(invalid_Ntrans_count)
     print(invalid_date_count)
 
-    df_cleaned = cleanData(df["DATE"])
-    # df_cleaned.to_csv("for-applicants/clean_sample.csv", index=False)
+    df_cleaned = cleanData(df)
+    df_cleaned.to_csv("for-applicants/clean_sample.csv", index=False)
 
     # creating the db and a cursor which is used to interact with the db
     connection = sqlite3.connect("outlet_transactions.db")
@@ -29,13 +29,38 @@ def loadData():
 
     connection.close()
 
-def cleanData(date):
+def cleanData(df):
     try:
-        return pd.to_datetime(date, format='%Y-%m-%d')
+        return pd.to_datetime(df["DATE"], format='%Y-%m-%d')
     except ValueError:
-        
-    
-    
+        indexesToDrop = []
+        for index, date in enumerate(df["DATE"]):
+            dateArray = date.split("-")
+            val1, val2, val3 = dateArray
+            
+            # if the first value isnt a year means bad data
+            if len(val1) <= 2:
+                #if both month and day are < 12 then its impossible to tell which one is which
+                if (int(val1) <= 12 and int(val2)) <= 12:
+                    print(index, date, 'DROP')
+                    # df.drop(index)
+                    indexesToDrop.append(index)
+                # if val1 > 12 that means its the day
+                elif int(val1) > 12:
+                    dateArray = [val3, val2, val1]
+                    date = "-".join(dateArray)
+                    df.at[index, "DATE"] = date
+                    # print(index, date)
+
+                # if val2 > 12 that means its the day 
+                elif int(val2) > 12:
+                    dateArray = [val3, val1, val2]
+                    date = "-".join(dateArray)
+                    df.at[index, "DATE"] = date
+                    # print(index, date)
+        df = df.drop(indexesToDrop)
+        return df
+                    
 
 # removes invalid dates that dont follow the year-month-day format
 def is_valid_date(date):
@@ -44,6 +69,5 @@ def is_valid_date(date):
         return False
     except ValueError:
         return True
-    
 
 loadData()
